e929b49e60af27acef648178c84cfc53698d9b7a
C Selvaraju <trainerselva@gmail.com>
2024-02-28 02:00:03 +0530

    first commit

--- movies/urls.py
+++ movies/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index),
-    path("<int:id>", views.movie_detail, name="movie-detail"),
+    # path("<int:id>", views.movie_detail, name="movie-detail"),
+    path("<slug:slug>", views.movie_detail, name="movie-detail"),
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import Http404
 
+from django.shortcuts import get_object_or_404
+
 # Create your views here.
 
 # Get all the models to use
@@ -13,12 +15,29 @@ def index(request):
     })
 
 
-def movie_detail(request, id):
-    try:
-        movie = Movie.objects.get(id=id)
-    except:
-        raise Http404
+# def movie_detail(request, id):
+#     # try:
+#     #     # movie = Movie.objects.get(id=id)
+
+#     #     # Alternate method of getting the required object
+#     #     # Here pk indicates primary key
+
+#     #     movie = Movie.objects.get(pk=id)
+#     # except:
+#     #     raise Http404
     
+#     movie = get_object_or_404(Movie, pk=id)
+
+#     return render(request, "movies/movie_detail.html", {
+#         "title": movie.title,
+#         "mainactor": movie.mainactor,
+#         "rating": movie.rating,
+#         "is_boxofficehit": movie.is_boxofficehit
+#     })
+
+def movie_detail(request, slug):
+    movie = get_object_or_404(Movie, slug=slug)
+
     return render(request, "movies/movie_detail.html", {
         "title": movie.title,
         "mainactor": movie.mainactor,
